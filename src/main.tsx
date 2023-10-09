779111269cc695f4f919312238d5ca2e63e48f96
lulek1410 <jan.szewczynski@gmail.com>
2023-10-09 13:51:35 +0200

    Add gh-pages deploy and remove unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import "./index.css";
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import { Provider } from "react-redux";
+import App from "./App.tsx";
+import "./index.css";
 import { store } from "./store/index.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
